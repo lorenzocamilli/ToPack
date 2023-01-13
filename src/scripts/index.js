ec3924908cd54ca6d21b288d5ff6c7cfd2ad8dba
lorenzo camilli <lorcamilli@gmail.com>
2023-01-13 15:04:02 +0100

    Add userAddress in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ const Web3 = require('web3');
 const fs = require('fs')
 web3 = new Web3("http://127.0.0.1:7545")   // connect to ganache
 const contractAddress = '0x309E4a87986FBFF10c17BF5F227a7307EF7d64C1'	// CHANGE THIS
+const  userAddress = '0x3bC93bd5e2E9c07aACE7f281b6f149D6A03a0f2b'	// CHANGE THIS
 
 async function web3Contract() {
 	const path = require('path');
@@ -11,8 +12,8 @@ async function web3Contract() {
 
 	const postID = 0	// placeholder of the post, necessary only to call the changeDeliveryTime and deletePost methods
 	const newDeliveryTime = 10 // placeholder of the post, necessary only to call the deletePost method
-	Pack.methods.getUserPosts().call({from:accounts[0]});
-	Pack.methods.createPost(111111111).send({from: userAddress, gasLimit:300000 });
+	Pack.methods.getUserPosts().call((err, result) => { console.log(result) })	//Pack.methods.createPost(111111111).send({from: userAddress, gasLimit:300000 });
+	//Pack.methods.createPost(111111111).send({from: userAddress, gasLimit:300000 });
 	//Pack.methods.deletePost(postID).send({from: userAddress, gasLimit:300000 });
 	//Pack.methods.changeDeliveryTime(postID, newDeliveryTime).send({from: userAddress, gasLimit:300000 });
 }
