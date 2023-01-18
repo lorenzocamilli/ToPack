25036ec3588cf56580d5e570c06f0232682518c5
lorenzo camilli <lorcamilli@gmail.com>
2023-01-18 10:12:39 +0100

    Fixed erros of frontend

--- src/index.js
+++ src/index.js
@@ -1,39 +1,78 @@
-const Web3 = require('web3');
-web3 = new Web3("http://127.0.0.1:7545")   // connect to ganache
-const contractAddress = '0xfd48F8c5e1109feb878d353F4E2F25aB72F03776'	// CHANGE THIS
-//const  userAddress = '0x3bC93bd5e2E9c07aACE7f281b6f149D6A03a0f2b'	// CHANGE THIS
+// Prevent forms from submitting and reloading the page
+$("form").submit(function(e){e.preventDefault();});
+
+// Set the contract address
+var contractAddress = '0x110aa4fc7cAA8A031Af36e5bFfcD16d15E81FEb9';
+// Set the relative URI of the contract’s skeleton (with ABI)
+var contractJSON = "build/contracts/BitMathGame.json"
+// Set the sending address
 var senderAddress = '0x0';
+// Set contract ABI and the contract
 var contract = null;
 
+$(window).on('load', function() {
+  initialise(contractAddress);
+});
 
-async function web3Contract() {
-	const path = require('path');
-	const abi = require(path.join(__dirname, './build/contracts/Pack.json'));
-    console.log(abi);
+// Asynchronous function (to work with modules loaded on the go)
+// For further info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/async_function
+async function initialise(contractAddress) {
+  // Initialisation of Web3
+	if (typeof web3 !== 'undefined') {
+	  web3 = new Web3(web3.currentProvider);
+	} else {
+	  // Set the provider you want from Web3.providers
+    // Use the WebSocketProvider to enable events subscription.
+	  web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:7545"));
+	}
 
-    const contract = new web3.eth.Contract(abi, contractAddress);
-    console.log(contract)
-    
-    const accounts = await web3.eth.getAccounts();
-	console.log(accounts[0])
+  // Load the ABI. We await the loading is done through "await"
+  // More on the await operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+  await $.getJSON(contractJSON,
+    function( contractData ) { // Use of IIFEs: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+      // console.log(contractAbi);
+      contract = new web3.eth.Contract(contractData.abi, contractAddress);
+    }
+  ).catch((error) => { console.error(error); });
+  // Arrow funcction expression at work. For further info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+  if (!contract) {
+    console.error("No contract loaded.");
+    return false;
+  }
+
+	// Set the address from which transactions are sent
+	accounts = await web3.eth.getAccounts();
+	// console.log(accounts[0])
 	senderAddress = accounts[0]
 	console.log("Sender address set: " + senderAddress)
 
-//	const postID = 15	// placeholder of the post, necessary only to call the changeDeliveryTime and deletePost methods
-//	const newDeliveryTime = 10 // placeholder of the post, necessary only to call the deletePost method
-//	 	contract.methods.getUserPosts().call((err, result) => { console.log(result) })	//contract.methods.createPost(111111111).send({from: userAddress, gasLimit:300000 });
-	contract.methods.createPost(111111111).send({from: userAddress, gasLimit:300000 });
-//	contract.methods.deletePost(postID).send({from: userAddress, gasLimit:300000 });
-	//contract.methods.changeDeliveryTime(postID, newDeliveryTime).send({from: userAddress, gasLimit:300000 });
+	// Subscribe to all events by the contract
+	contract.events.allEvents(
+	callback=function(error, event){ // A "function object". Explained here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions#The_function_expression_(function_expression)
+      if (error) {
+        console.error(error)
+      }
+      console.log(event);
+  });
+
+
 }
 
-async function run() {
-    try {
-        await web3Contract();
-    } catch (err) {
-        console.log(err);
-    } 
-}
 
-run();
 
+function createPost() {
+    var cost = $('#costInput').val();
+    if (cost < 1) {
+      alert("The given guess should be higher than 0");
+      return false;
+    }
+  
+    console.log("Provided cost is: " + cost);
+  
+    contract.methods.createPost(cost).send({ from: senderAddress, gasLimit: 300000 }).then(function (result) {
+      console.log("Price sent: " + cost);
+    })
+  
+    return false;
+  }
