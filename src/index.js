c3dc1f59e6f5fcab49ef5912ef9e4df781bf81bb
lorenzo camilli <lorcamilli@gmail.com>
2023-02-07 08:37:44 +0100

    Updated also index files with new conf.js structure

--- src/index.js
+++ src/index.js
@@ -1,56 +1,16 @@
+var userAddress
+var contract
+var contractAddress
 
-var contractAddress = exportContract();
-var contractJSON = exportAbi();
-var userAddress = '0x0';
-var contract = null;
+$(window).on('load', function () {
+  start();
+});
 
-async function run() {
-  if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
-  }
-
-  await window.ethereum.enable();
-
-  const accounts = await ethereum.request({
-    method: 'eth_requestAccounts',
-  });
-  initialise(contractAddress, accounts)
+function start() {
+  run()
+  setTimeout(function () {
+    userAddress = exportUserAddr();
+    contractAddress = exportContractAddr();
+    contract = exportContract();
+  }, 500);
 }
-
-
-async function initialise(contractAddress, accounts) {
-
-  if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:7545"));
-  }
-
-  await $.getJSON(contractJSON,
-    function (contractData) {
-      contract = new web3.eth.Contract(contractData.abi, contractAddress);
-    }
-  ).catch((error) => { console.error(error); });
-
-  if (!contract) {
-    console.error("No contract loaded.");
-    return false;
-  }
-  userAddress = accounts[0]
-  console.log("Sender address set: " + userAddress)
-
-  contract.events.allEvents(
-    callback = function (error, event) {
-      if (error) {
-        console.error(error)
-      }
-      console.log(event);
-    });
-}
-
-
-
-
-
-
-
